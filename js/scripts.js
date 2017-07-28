7b8187830894e6cd1ae8ef014f4ecf6a82985e0e
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-07-28 08:56:21 +0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var a = prompt('Podaj wartość a:'),
+    b = prompt('Podaj wartość b:');
+
+var value = (a * a) + (2 * a * b) - (b * b);
+
+if (value > 0) {
+	console.log('Wynik dodatni.');
+} else if (value < 0) {
+	console.log('Wynik ujemny.');	
+} else {
+	console.log('Wynik równy zero.');
+}
