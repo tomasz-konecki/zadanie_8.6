d1e53388687752301da5a80c0b85d177605bc2dc
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-07-29 21:01:44 +0200

    changed tabs to spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 var a = prompt('Podaj wartość a:'),
-	b = prompt('Podaj wartość b:'),
-	result = '',
-	x = document.getElementById('equation');
+....b = prompt('Podaj wartość b:'),
+....result = '',
+....x = document.getElementById('equation');
 
 var value = (a * a) + (2 * a * b) - (b * b);
 
 if (value > 0) {result = 'dodatni';}
-	else if (value < 0) {result = 'ujemny';}
-		else {result = 'równy zero';}
+....else if (value < 0) {result = 'ujemny';}
+........else {result = 'równy zero';}
 
 x.innerHTML = result;
