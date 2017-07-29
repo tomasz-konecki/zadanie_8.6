517881ee2362649f1fa9f46c9b628e2a04874dac
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-07-29 21:11:31 +0200

    corrected 'var' declaration

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 var a = prompt('Podaj wartość a:'),
     b = prompt('Podaj wartość b:'),
     result = '',
-    x = document.getElementById('equation');
-
-var value = (a * a) + (2 * a * b) - (b * b);
+    x = document.getElementById('equation'),
+    value = (a * a) + (2 * a * b) - (b * b);
 
 if (value > 0) {result = 'dodatni';}
     else if (value < 0) {result = 'ujemny';}
